534cbdcd7bc74d4e30bf5f38d50fb0a37e28fba4
Frank Löffler <knarf@cct.lsu.edu>
2016-09-16 18:19:04 +0100

    erste Vorlage

---
+++ header2.js
@@ -0,0 +1,13 @@
+document.write(
+"    <script src='https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js'></script>"+
+"    <script src='bootstrap/js/bootstrap.min.js'></script>"+
+"    <!-- Bootstrap core CSS -->"+
+"    <link href='bootstrap/css/bootstrap.min.css' rel='stylesheet'>"+
+"    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->"+
+"    <!--[if lt IE 9]>"+
+"      <script src='https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js'></script>"+
+"      <script src='https://oss.maxcdn.com/respond/1.4.2/respond.min.js'></script>"+
+"    <![endif]-->"+
+"    <style>body { padding-top: 70px; }</style>"+
+"    <link rel='stylesheet' type='text/css' href='derse.css' />"+
+"");
